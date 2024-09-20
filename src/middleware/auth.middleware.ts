a0d3e34e4f5b7e34f09f1552f92a7a15d9265c32
MandySpaan <mandyspaan94@gmail.com>
2024-09-20 15:54:44 +0200

    feat: isAdmin middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import User from "../database/entities/users/user.model";
 
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key";
 // The "your_jwt_secret" can be set as a fallback value in case the .env is not set
@@ -27,4 +28,20 @@ export function authToken(req: AuthRequest, res: Response, next: NextFunction) {
     req.user = user as { userId: string; email: string };
     next();
   });
+}
+
+export async function isAdmin(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const userId = req.user?.userId;
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  if (user.role !== "admin" && user.role !== "superAdmin") {
+    return res.status(403).send({ message: "Unauthorized" });
+  }
+  next();
 }

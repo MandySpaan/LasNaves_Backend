1c21485cf7638afde6eff3fa41df0f9c42c6578d
MandySpaan <mandyspaan94@gmail.com>
2024-09-26 20:06:19 +0200

    feat: cors installation

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "express-async-errors";
 import "dotenv/config";
 import { dbConnection } from "./database/db";
@@ -9,6 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/healthy", (req, res) => {
   res.json({
